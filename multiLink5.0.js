6fc0e76c476ae4c53a9b6f6e0b37cb689eb93b22
sjosep419 <sjosep34@uic.edu>
2022-11-25 16:52:56 -0600

    oops

--- multiLink5.0.js
+++ multiLink5.0.js
@@ -1,4 +1,4 @@
-function pieChart() {
+function pieChart(data) {
     // set the dimensions and margins of the graph
     var width = 450
     height = 450
@@ -59,12 +59,12 @@ function pieChart() {
 
     }
     // Initialize the plot with the first dataset
-    pieupdate(data1)
+    pieupdate(data)
 }
 
 
 function lineChart() {
-    // create 2 data_set
+    // // create 2 data_set
     // var data1 = {a: 9, b: 20, c:30, d:8, e:12}
     // var data2 = {a: 6, b: 16, c:20, d:14, e:19, f:12}
     // var data3 = {a: 2, b: 4, c:2, d:4, e:1, f:6}
@@ -173,7 +173,7 @@ function lineChart() {
 }
 
 function init() {
-    pieChart();
+    pieChart(data1);
     lineChart();
 }
 window.onload = init;

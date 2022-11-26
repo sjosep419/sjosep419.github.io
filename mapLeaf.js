81249fc5535fb20b2a619fec556b9d31611e4012
kmahes5 <kmahes5@uic.edu>
2022-11-26 00:13:12 -0600

    change coords

--- mapLeaf.js
+++ mapLeaf.js
@@ -1,6 +1,6 @@
 function createMap() {
     
-    var map = L.map('map').setView([-41.3058, 174.82082], 8);
+    var map = L.map('map').setView([41.8781, 87.6298], 5);
     mapLink = 
         '<a href="http://openstreetmap.org">OpenStreetMap</a>';
     L.tileLayer(

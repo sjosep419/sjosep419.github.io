d010fff28aead9469b2c1cb5720ff2e846418f43
sjosep419 <sjosep34@uic.edu>
2022-11-27 00:35:17 -0600

    updates

--- pieChartData.js
+++
@@ -1,3 +0,0 @@
-var data1 = {a: 9, b: 20, c:30, d:8, e:12}
-var data2 = {a: 6, b: 16, c:20, d:14, e:19, f:12}
-var data3 = {a: 2, b: 4, c:2, d:4, e:1, f:6}
---
+++ pieChartData1.js
@@ -0,0 +1,3 @@
+var data1 = {members: 3278, casual: 1304}
+var data2 = {members: 6756, casual: 3373}
+var data3 = {members: 1522, casual: 1345}

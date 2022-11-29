919121dbae244e8fc4dd32ee9ced5147dfbecb9d
kmahes5 <kmahes5@uic.edu>
2022-11-28 21:41:44 -0600

    both single

--- seperategraph.js
+++ seperategraph.js
@@ -8,11 +8,22 @@ new Runtime().module(define, name => {
 
 
 
+import {Runtime, Inspector} from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@5/dist/runtime.js";
+import define from "https://api.observablehq.com/d/ffb3099e2ba5dd91.js?v=3";
+new Runtime().module(define, name => {
+  if (name === "chart2") return new Inspector(document.querySelector("#observablehq-chart2-fc7e868f"));
+  return ["focus2"].includes(name);
+});
+
+
+
+
+
 function display2018() {
     Document.getElementById('viz').innerHTML = html`<div id="observablehq-chart1-62198941"></div>`;
 }
 
 
-// function display2020() {
-//     Document.getElementById('viz').innerHTML = html`<div id="observablehq-chart2-b580c0ef"></div>`;
-// }
+function display2020() {
+    Document.getElementById('viz').innerHTML = html`<div id="observablehq-chart2-fc7e868f"></div>`;
+}

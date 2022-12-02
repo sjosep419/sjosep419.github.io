e94890471bb544e8959140124521017dd1234cc6
sjosep419 <sjosep34@uic.edu>
2022-12-02 15:10:11 -0600

    fixed margins again

--- newmultiLink2.js
+++ newmultiLink2.js
@@ -84,7 +84,7 @@ function lineChart() {
 
 
     // set the dimensions and margins of the graph
-    var margin = {top: 10, right: 100, bottom: 30, left: 25},
+    var margin = {top: 10, right: 100, bottom: 30, left: 40},
         width = 460 - margin.left - margin.right,
         height = 400 - margin.top - margin.bottom;
     

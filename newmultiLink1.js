b73b728083b8a10e9d940bcbdfbb58c8801b2f34
sjosep419 <sjosep34@uic.edu>
2022-12-02 15:08:09 -0600

    fixed spacing on newLinkedView

--- newmultiLink1.js
+++ newmultiLink1.js
@@ -84,7 +84,7 @@ function lineChart() {
 
 
     // set the dimensions and margins of the graph
-    var margin = {top: 10, right: 100, bottom: 30, left: 30},
+    var margin = {top: 10, right: 100, bottom: 30, left: 25},
         width = 460 - margin.left - margin.right,
         height = 400 - margin.top - margin.bottom;
     
